94576109d91f98fc03ce01fb79e89bd7a39fc5d4
David Antoon <david@frontegg.com>
2025-11-05 03:37:16 +0200

    improve openapi adapter config

--- libs/adapters/src/openapi/openapi.types.ts
+++ libs/adapters/src/openapi/openapi.types.ts
@@ -1,4 +1,21 @@
+import {AuthInfo} from "@modelcontextprotocol/sdk/server/auth/types.js";
+
 export interface OpenApiAdapterOptions {
   name: string;
   url: string;
+
+  additionalHeaders?: Record<string, string>;
+  headersMapper?: (authInfo: AuthInfo, headers: Record<string, string>) => Record<string, string>;
+  bodyMapper?: (authInfo: AuthInfo, body: any) => any;
+
+  /**
+   * This can be used to map request information to specific
+   * input schema values as required by the API.
+   * For example, mapping tenantId from authenticated session payload to
+   * a specific proprty in the input schema, this key will be hidden to mcp clients
+   * and filled by the adapter before sending the request to the API.
+   * @param authInfo
+   * @param request
+   */
+  inputSchemaMapper?: (inputSchema: any) => any;
 }
