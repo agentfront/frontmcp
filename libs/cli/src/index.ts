765b7a57108bb4b41dbb0dfeda36a3e911455cb0
David Antoon <david@frontegg.com>
2025-11-15 13:08:05 +0200

    fix(ci): add message to cli to identify running version in cici

--- libs/cli/src/index.ts
+++ libs/cli/src/index.ts
@@ -1,1 +1,6 @@
-export const name = 'forntmcp-cli'
+import {getSelfVersion} from "./version";
+
+const version = getSelfVersion();
+export const name = `frontmcp cli version ${version}`
+
+console.log(`Hello, I'm ${name}`)
